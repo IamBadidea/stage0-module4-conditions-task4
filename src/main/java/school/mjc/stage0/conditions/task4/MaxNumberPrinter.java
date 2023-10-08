08e44c23eaba3fadcaa1cba360d4fa8a8d87e40f
Philip <okruzhko.f@gmail.com>
2023-10-08 23:50:56 +0400

    Complete s0m4t4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        System.out.println(getMaxFromTwo(getMaxFromTwo(first, second), third));
+
+    }
+
+    private int getMaxFromTwo(int one, int two) {
+        return one > two || one == two ? one : two;
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -1,6 +1,12 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
+    private int result;
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        if (first > second) {
+            result = 10;
+        } else {
+            result = -10;
+        }
     }
 }
